498d48c1f98afbea2a5710ddce9f16ba3121c8d7
Rajinder77 <Rajindersandhu808@gmail.com>
2022-01-20 11:51:01 -0500

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome to travel form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class ="container">
+        <h3>welcome to kirtowal khurd</h3>
+        <p>enter your details</p>
+    <form action="index2.php" method="post">
+    <input type="text" name="name" id="name" placeholder="enter your name">
+    <input type="text" name="age" id="name" placeholder="enter your age">
+    <input type="text" name="gender" id="name" placeholder="enter your gender">
+    <input type="email" name="email" id="email" placeholder="enter the email">
+    <input type="text" name="phone" id="phone" placeholder="enter the phone">
+<textarea name="desc" id="desc" cols="30" rows="10" placeholder="enter any other detail here"></textarea>
+    <button class="btn">submit</button>
+    <button class="btn">reset</button>
+    </form>
+        
+</div>
+<script src="java.js"></script>
+
+</body>
+</html>
